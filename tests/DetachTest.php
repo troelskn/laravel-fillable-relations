44a976cc1af95853f9cf9cea236485dd2c12ee38
Alexey Ivanov <lexuzieel@gmail.com>
2019-08-29 04:45:19 +0300

    Add tests

---
+++ tests/DetachTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace LaravelFillableRelations\Tests;
+
+use LaravelFillableRelations\Tests\Models\Vehicle;
+use LaravelFillableRelations\Tests\Models\Wheel;
+
+class DetachTests extends TestCase
+{
+    protected $vehicle;
+    
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->vehicle = Vehicle::create(
+            [
+                'name' => 'My nice car',
+            ]
+        );
+
+        for ($i = 0; $i < 4; $i++) {
+            Wheel::create(
+                [
+                    'car' => $this->vehicle,
+                    'size' => 24,
+                ]
+            );
+        }
+    }
+
+    public function testDetachWithEmptyArray()
+    {
+        $this->assertEquals(4, count($this->vehicle->wheels->toArray()));
+        
+        $this->vehicle->fill([
+            'wheels' => [],
+        ])->save();
+
+        $this->vehicle->refresh();
+
+        $this->assertEquals(0, count($this->vehicle->wheels->toArray()));
+    }
+
+    public function testDoNothingWithNullValue()
+    {
+        $this->assertEquals(4, count($this->vehicle->wheels->toArray()));
+        
+        $this->vehicle->fill([
+            'wheels' => null,
+        ])->save();
+
+        $this->vehicle->refresh();
+
+        $this->assertEquals(4, count($this->vehicle->wheels->toArray()));
+    }
+}
